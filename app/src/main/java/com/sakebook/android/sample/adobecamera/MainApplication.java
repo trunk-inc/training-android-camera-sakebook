e43be48c2f5c09e6ea58f999f9e279ca8b118c91
Shinya Sakemoto <sakebook@gmail.com>
2016-03-05 19:36:02 +0900

    Add Mainapplication class

---
+++ app/src/main/java/com/sakebook/android/sample/adobecamera/MainApplication.java
@@ -0,0 +1,27 @@
+package com.sakebook.android.sample.adobecamera;
+
+import android.app.Application;
+
+import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
+import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
+
+/**
+ * Created by sakemotoshinya on 16/03/05.
+ */
+public class MainApplication extends Application implements IAdobeAuthClientCredentials {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AdobeCSDKFoundation.initializeCSDKFoundation(getApplicationContext());
+    }
+
+    @Override
+    public String getClientID() {
+        return BuildConfig.CREATIVE_SDK_CLIENT_ID;
+    }
+
+    @Override
+    public String getClientSecret() {
+        return BuildConfig.CREATIVE_SDK_CLIENT_SECRET;
+    }
+}
