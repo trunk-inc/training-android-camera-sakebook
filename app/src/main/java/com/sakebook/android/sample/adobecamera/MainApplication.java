8570c47376e43e2d62508c04a4ae1842681e9a68
Shinya Sakemoto <sakebook@gmail.com>
2016-03-05 20:29:43 +0900

    Import Aviary

--- app/src/main/java/com/sakebook/android/sample/adobecamera/MainApplication.java
+++ app/src/main/java/com/sakebook/android/sample/adobecamera/MainApplication.java
@@ -2,13 +2,14 @@ package com.sakebook.android.sample.adobecamera;
 
 import android.app.Application;
 
+import com.adobe.creativesdk.aviary.IAviaryClientCredentials;
 import com.adobe.creativesdk.foundation.AdobeCSDKFoundation;
 import com.adobe.creativesdk.foundation.auth.IAdobeAuthClientCredentials;
 
 /**
  * Created by sakemotoshinya on 16/03/05.
  */
-public class MainApplication extends Application implements IAdobeAuthClientCredentials {
+public class MainApplication extends Application implements IAviaryClientCredentials {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -23,5 +24,10 @@ public class MainApplication extends Application implements IAdobeAuthClientCred
     @Override
     public String getClientSecret() {
         return BuildConfig.CREATIVE_SDK_CLIENT_SECRET;
+    }
+
+    @Override
+    public String getBillingKey() {
+        return ""; // Leave this blank
     }
 }
